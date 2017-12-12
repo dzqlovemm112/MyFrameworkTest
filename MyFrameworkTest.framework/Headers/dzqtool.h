1c00c4fe0ce594a3b83c9dfa8409d5d4ea50ba64
dzqlovemm112 <875957535@qq.com>
2017-12-12 17:25:22 +0800

    添加打包好的framework

---
+++ MyFrameworkTest.framework/Headers/dzqtool.h
@@ -0,0 +1,13 @@
+//
+//  dzqtool.h
+//  MyFrameworkTest
+//
+//  Created by 罗海方 on 2017/12/12.
+//  Copyright © 2017年 罗海方. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface dzqtool : NSObject
++(void)saybig;
+@end
